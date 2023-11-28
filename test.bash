198a32e1b180009abae6c1ab6091c691ab02692e
Naoki Otsubo <s22c1019dg@s.chibakoudai.jp>
2023-11-29 06:05:19 +0900

    Improve test bug

--- test.bash
+++ test.bash
@@ -21,14 +21,14 @@ out=$(seq 5 | ./plus)
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]  || ng ${LINENO}
-[ "${out}" = "" ] ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo  | ./plus)
 [ "$?" = 1 ]   || ng ${LINENO}
 [ "${out}" = "" ]  || ng ${LINENO}
 
 
-[ "$res" = 0] && echo OK
+[ "$res" = 0 ] && echo OK
 
 exit $res
 
